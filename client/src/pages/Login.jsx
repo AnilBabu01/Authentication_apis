59cbb21712a1bd8f14f82a1d756aa5f2d9d61468
anil babu <anilb9850@gmail.com>
2022-07-09 11:22:06 +0530

    integrate login and add logged user info

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,115 @@
+import React, { useState ,useEffect} from "react";
+import axios from "axios";
+const Login = () => {
+  const [data, setdata] = useState({
+    email: "",
+    password: "",
+  });
+  const [info, setinfo] = useState({})
+  const { email, password } = data;
+  const handleinput = (e) => {
+    setdata({ ...data, [e.target.name]: e.target.value });
+  };
+ 
+  
+  useEffect(() => {
+    getuserinfo();
+   }, [])
+   axios.defaults.headers.get["Authorization"] = `Bearer ${localStorage.getItem(
+    "authtoken"
+  )}`;
+
+ 
+  const getuserinfo = async()=>{
+    try {
+         const res = await axios.get("http://localhost:8080/api/auth/getuserinfo",);
+        console.log("data user loggr sr",res.data);
+        setinfo(res.data)
+      } catch (error) {}
+  }
+
+
+  const onsubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post("http://localhost:8080/api/auth/login", {
+        email: email,
+        password: password,
+      });
+      console.log(res.data.authtoken);
+      localStorage.setItem("authtoken",res.data.authtoken)
+     
+    } catch (error) {}
+  };
+
+  const logout =()=>{
+    localStorage.removeItem("authtoken");
+
+    console.log("logout successfully")
+  }
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <h2>login here</h2>
+
+          <form style={{ width: "200px" }} onSubmit={onsubmit}>
+            <label htmlFor="classs">enter your email</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Enter your email ..."
+              value={email}
+              onChange={handleinput}
+            />
+            <label htmlFor="city">Enter your city</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password ..."
+              value={password}
+              onChange={handleinput}
+            />
+
+            <input type="submit" value="login" />
+          </form>
+        </div>
+        <div>
+       { info && info.map((item)=>{
+        return(
+            <>
+                 <p>this is your city name</p>
+                 <p>{item.city}</p>
+                 <p>this is your course name</p>
+                 <p>{item.classs}</p>
+                 <p>this is your fathers name</p>
+                 <p>{item.fathersname}</p>
+                 <p>this is your state name</p>
+                 <p>{item.state}</p>
+                 
+            </>
+        )
+       })}
+      
+      <button onClick={logout}>logout</button>
+            
+      </div>
+      </div>
+
+
+      
+    </>
+  );
+};
+
+export default Login;
--- client/src/pages/Uploaimg.jsx
+++ client/src/pages/Uploaimg.jsx
@@ -1,45 +1,56 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import axios from "axios";
+import Login from './Login'
 const Uploaimg = () => {
   const [data, setdata] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    password: "",
-  
+    classs: "",
+    city: "",
+    fathersname: "",
+    state: "",
   });
 
-  const [profilepic, setprofilepic] = useState("")
-  const { name, email, phone, password, } = data;
+ 
+  const [profilepic, setprofilepic] = useState("");
+  const { classs, city, fathersname, state } = data;
   const handleinput = (e) => {
     setdata({ ...data, [e.target.name]: e.target.value });
   };
-
+ console.log(info)
   const uploadimg = (e) => {
     console.log("aaaaa", e.target.files[0]);
-    setprofilepic(e.target.files[0] );
-   
+    setprofilepic(e.target.files[0]);
   };
+
+  
+
+  axios.defaults.headers.post["Authorization"] = `Bearer ${localStorage.getItem(
+    "authtoken"
+  )}`;
+
 
   const onsubmit = async (e) => {
     e.preventDefault();
     console.log("pro", profilepic);
-   
+
     try {
       const formdata = new FormData();
-      formdata.append('myFile',profilepic,profilepic.name)
-      formdata.append('name',name)
-      formdata.append('email',email)
-      formdata.append('phone',phone)
-      formdata.append('password',password)
-      console.log(formdata )
-      
-      const res = await axios.post("http://localhost:8080/api/auth/register",formdata);
-      console.log(res)
+      formdata.append("myFile", profilepic, profilepic.name);
+      formdata.append("classs", classs);
+      formdata.append("city", city);
+      formdata.append("fathersname", fathersname);
+      formdata.append("state", state);
+      console.log(formdata);
+
+      const res = await axios.post(
+        "http://localhost:8080/api/auth/info",
+        formdata
+      );
+      console.log(res);
     } catch (error) {}
   };
   return (
     <>
+      <Login/>
       <div
         style={{
           display: "flex",
@@ -51,35 +62,45 @@ const Uploaimg = () => {
           <h2>uxpload your profile image using multer</h2>
 
           <form style={{ width: "200px" }} onSubmit={onsubmit}>
-            <label htmlFor="name">name</label>
+            <label htmlFor="classs">enter your class</label>
             <input
               type="text"
-              id="name"
-              name="name"
-              placeholder="Enter your name ..."
-              value={name}
+              id="classs"
+              name="classs"
+              placeholder="Enter your class ..."
+              value={classs}
               onChange={handleinput}
             />
-            <label htmlFor="email">Email</label>
+            <label htmlFor="city">Enter your city</label>
             <input
               type="text"
-              id="email"
-              name="email"
-              placeholder="Enter your email ..."
-              value={email}
+              id="city"
+              name="city"
+              placeholder="Enter your city ..."
+              value={city}
               onChange={handleinput}
             />
-            <label htmlFor="phone">phone number</label>
+            <label htmlFor="phone">Enter father's name</label>
             <input
               type="text"
-              id="phone"
-              name="phone"
-              placeholder="Enter your phone ..."
-              value={phone}
+              id="fathersname"
+              name="fathersname"
+              placeholder="Enter your father's name..."
+              value={fathersname}
               onChange={handleinput}
             />
 
-            <label htmlFor="number">select your file</label>
+            <label htmlFor="state">Enter your status</label>
+            <input
+              type="text"
+              id="state"
+              name="state"
+              placeholder="Enter your password ..."
+              value={state}
+              onChange={handleinput}
+            />
+
+            <label htmlFor="number">select your profile photo</label>
 
             <input
               type="file"
@@ -88,16 +109,6 @@ const Uploaimg = () => {
               accept="images/*"
               name="file"
               onChange={uploadimg}
-            />
-
-            <label htmlFor="password">password</label>
-            <input
-              type="text"
-              id="password"
-              name="password"
-              placeholder="Enter your password ..."
-              value={password}
-              onChange={handleinput}
             />
             <input type="submit" value="register" />
           </form>
